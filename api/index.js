0757d73b26815bf2dc7db1941508b2a42342672e
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-09-27 18:49:37 +0530

    Trying out vercel api

---
+++ api/index.js
@@ -0,0 +1,4 @@
+module.exports = async (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    return res.send(`<h1>Hello world</h1> ${JSON.stringify(req)}`);
+};
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,3 @@
 #! /usr/bin/env node
 
-console.log("hello cli");
+require("../index.js")
