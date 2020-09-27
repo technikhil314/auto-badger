39a711428a002ea1083f0f8a71e01d595b9d3c41
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-09-27 20:42:47 +0530

    adding new netlify function

---
+++ functions/badgeme.js
@@ -0,0 +1,40 @@
+const http = require("http");
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            const { statusCode } = res;
+            if (statusCode !== 200) {
+                console.error(error.message);
+                res.resume();
+                reject("Something wrong with shields.io");
+                return;
+            }
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    console.log(rawData);
+                    resolve(rawData);
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+            reject("Something wrong with shields.io");
+        });
+    })
+}
+
+module.exports = async (event) => {
+    console.log(event.headers.referer || event.headers.Referer);
+    const badges = await Promise.all([fetch("http://img.shields.io/badge/style-for--the--badge-green?logo=appveyor&style=for-the-badge")]);
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "image/svg+xml"
+        },
+        body: badges.join(" "),
+    }
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 ![whaterver placeholder](https://auto-badger-seven.vercel.app/api)
+![whaterver placeholder](https://auto-badger.netlify.app/.netlify/functions/badgeme)
