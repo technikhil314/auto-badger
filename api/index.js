fc534db4efaa1b75d9fda04d981a1d8b98661c4c
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-09-27 20:07:27 +0530

    Demo shields.io badge

--- api/index.js
+++ api/index.js
@@ -1,4 +1,35 @@
+const http = require("http");
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            const { statusCode } = res;
+            if (statusCode !== 200) {
+                console.error(error.message);
+                res.resume();
+                reject("Something wrong with shields.io");
+                return;
+            }
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    console.log(rawData);
+                    resolve(rawData);
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+            reject("Something wrong with shields.io");
+        });
+    })
+}
+
 module.exports = async (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    return res.send(`from <h1>${req.headers.referer}</h1>`);
+    res.setHeader("Content-Type", "image/svg+xml");
+    console.log(req.headers.referer);
+    const badges = await Promise.all([fetch("http://img.shields.io/badge/style-for--the--badge-green?logo=appveyor&style=for-the-badge")]);
+    return res.send(badges.join(" "));
 };
